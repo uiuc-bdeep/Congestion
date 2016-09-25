eabc215f6aabc9d20055409eaadc9722e8c5bf59
Eric Rosenberg <ehaydenr@gmail.com>
2016-09-25 17:44:53 -0500

    print out env

--- pipelines/vot/run-analysis/run-analysis.sh
+++ pipelines/vot/run-analysis/run-analysis.sh
@@ -5,6 +5,8 @@ tar -xzf data-stores/stores.tar.gz
 tar -xzf data-stream/stream.tar.gz
 ls -l
 
+env
+
 Rscript "repo/pipelines/vot/Intraday VOT - limited survey-crawler diff - depart time 4 hours range.R"
 
 tar -czf analysis.tar.gz analysis

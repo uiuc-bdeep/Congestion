5df7c7daddbd3d1d9b06a9df6bb7af4cf6d7cffb
Eric Rosenberg <ehaydenr@gmail.com>
2016-10-03 18:45:02 -0500

    update pipeline

--- pipelines/vot/run-analysis/run-analysis.sh
+++ pipelines/vot/run-analysis/run-analysis.sh
@@ -6,10 +6,21 @@ tar -xzf data-stream/stream.tar.gz
 ls -l
 
 export BDEEP_PROD=1
+export BDEEP_R_ROOT=$PWD
+
+SCRIPTS=$PWD/repo/vot
 
 env
 
-Rscript "repo/pipelines/vot/Intraday VOT - limited survey-crawler diff - depart time 4 hours range.R"
+Rscript "$SCRIPTS/1.0 add car.time 0 to extended crawler.R"
+Rscript "$SCRIPTS/1.1 long data files.R"
+Rscript "$SCRIPTS/1.2 vot estimation.R"
+Rscript "$SCRIPTS/1.3 vot plot.R"
+Rscript "$SCRIPTS/1.4 assign vot to trips.R"
+Rscript "$SCRIPTS/1.5 week aggregation.R"
+
+cp -r views analysis/
+cp "intermediate/vot/travel time and vot - weekly.csv" analysis/
 
 tar -czf analysis.tar.gz analysis
 cp analysis.tar.gz analysis/
--- vot/environment.R
+++ vot/environment.R
@@ -4,15 +4,15 @@ localFlag <- !prodFlag && !rstudioServerFlag
 
 out.rootPath <- NULL
 if (prodFlag) {
-	out.rootPath <- Sys.getenv("BDEEP_VOT_ROOT")
+	out.rootPath <- Sys.getenv("BDEEP_R_ROOT")
 } else if (rstudioServerFlag) {
-	out.rootPath <- "~/share/projects/Congestion/"
+	out.rootPath <- "~/share/projects/Congestion"
 } else if (localFlag) {
-	out.rootPath <- "//141.142.209.255/share/projects/Congestion/"
+	out.rootPath <- "//141.142.209.255/share/projects/Congestion"
 }
 
 generatePath <- function(p) {
-	path <- paste(out.rootPath, p, sep="")
+	path <- file.path(out.rootPath, p)
 	return(path)
 }
 
@@ -24,3 +24,17 @@ pkgTest <- function(x)
     if(!require(x, character.only = TRUE)) stop("Package not found")
   }
 }
+
+# Make sure all directory structures are setup
+paths = c(
+					"intermediate/vot/extended-crawler trips/long data/",
+					"intermediate/vot/choice model outputs/",
+					"views/tables/",
+					"views/figures/vot/"
+					)
+
+createPath <- function(p) {
+	dir.create(generatePath(p), showWarnings = FALSE, recursive = TRUE)
+}
+
+lapply(paths, createPath)
